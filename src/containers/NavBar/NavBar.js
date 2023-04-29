635f97401d8b953fd24899cabd381100f130c213
chenyunwen <sssh10110@gmail.com>
2023-04-30 03:36:17 +0800

    add navbar

--- src/containers/NavBar/NavBar.js
+++ src/containers/NavBar/NavBar.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { NavLink as RouterNavLink } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button } from 'reactstrap'; 
 import {
   Collapse,
   Navbar,
@@ -16,6 +18,7 @@ import {
 } from 'reactstrap';
 
 import Logo from '../../assets/images/logo.png'
+import Cart from '../../assets/images/cart.png'
 import appRoutes from '../../shared/appRoutes';
 import './NavBar.css';
 
@@ -26,45 +29,48 @@ function Example(args) {
 
   return (
     <div>
-      
+      <hr className='nav-red-line'/>
       <Navbar>
-        <hr className='nav-red-line'/>
-
-        <NavbarBrand className='nav-logo' tag={RouterNavLink} to={appRoutes.home}>
-          <img src={Logo} alt='logo' style={{ height: '80px' }} />
+        <NavbarBrand tag={RouterNavLink} to={appRoutes.home}>
+          <img src={Logo} alt='logo' className='nav-logo'/>
         </NavbarBrand>
-
-        {/* <NavbarBrand href="/">reactstrap</NavbarBrand> */}
+        <NavbarText className="headerStyle">Scotty Shirts U Illustrate (SSUI)</NavbarText>
+        <NavbarBrand className='shopping-cart' tag={RouterNavLink} to={appRoutes.home}>
+          <img src={Cart} alt='cart' style={{ height: '40px'}} />
+          <span>0</span>
+        </NavbarBrand>
+      </Navbar>
+      
+      <Navbar expand='md'>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
-          <Nav className="me-auto" navbar>
-            <NavItem>
+          <Nav className="md-auto" navbar>
+            <NavItem className='menu-font'>
               <NavLink tag={RouterNavLink} to={appRoutes.home} exact="true">
                 T-SHIRTS
               </NavLink>
             </NavItem>
-            <NavItem>
+            <NavItem className='menu-font'>
               <NavLink tag={RouterNavLink} to={appRoutes.not_implemented}>
                 CREATE FROM PICTURE
               </NavLink>
             </NavItem>
-            <NavItem>
+            <NavItem className='menu-font'>
               <NavLink tag={RouterNavLink} to={appRoutes.not_implemented}>
                 CREATE YOUR OWN
               </NavLink>
             </NavItem>
-            <NavItem>
+            <NavItem className='menu-font'>
               <NavLink tag={RouterNavLink} to={appRoutes.not_implemented}>
                 ABOUT US
               </NavLink>
             </NavItem>
-            <NavItem>
+            <NavItem className='menu-font'>
               <NavLink tag={RouterNavLink} to={appRoutes.not_implemented}>
                 LOG IN
               </NavLink>
             </NavItem>
           </Nav>
-          <NavbarText>Simple Text</NavbarText>
         </Collapse>
       </Navbar>
     </div>
